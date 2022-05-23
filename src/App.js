535c6cb141c11343b1c7ad89ad647d689d3345e1
Martin Farres <91481417+Tats01@users.noreply.github.com>
2022-05-23 13:40:07 -0300

    Code Review

--- src/App.js
+++ src/App.js
@@ -5,59 +5,7 @@ import './App.css';
 import { Header } from './components/Header/Header';
 
 function App() {
-<<<<<<< HEAD
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className='App-link'
-            href='https://reactjs.org/'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className='App-link'
-            href='https://redux.js.org/'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className='App-link'
-            href='https://redux-toolkit.js.org/'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className='App-link'
-            href='https://react-redux.js.org/'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
-=======
 	return <Header />;
->>>>>>> 92e759d3835d2d04c8d5074fce5be3554c92c0eb
 }
 
 export default App;
