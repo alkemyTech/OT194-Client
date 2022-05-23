a079b493862e1c3a5f7891276f9d1eb272693dc9
Martin Farres <91481417+Tats01@users.noreply.github.com>
2022-05-23 13:36:56 -0300

    Code Review

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,15 @@ module.exports = {
   corePlugins: {
     preflight: false,
   },
-  content: ['./src/**/*.{html,js,jsx}'],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {
       colors: {
-        redOng: '#DB5752',
-        yellowOng: '#FAFA88',
-        blueOng: '#9AC9FB',
+        'redOng': '#DB5752',
+        'yellowOng': '#FAFA88',
+        'blueOng': '#9AC9FB',
       },
     },
   },
   plugins: [],
-};
+}
