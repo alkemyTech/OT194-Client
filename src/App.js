e5d6b1e92df013008a89df7f9727026c4e69802b
Lali-DMI <lgarribas@dminc.com>
2022-05-23 23:59:07 -0300

    OT194-23-Formulario Registro

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React from 'react';
-// import logo from './logo.svg';
-// import { Counter } from './features/counter/Counter';
-import './App.css';
-import { Header } from './components/Header/Header';
+import React from "react";
+import "./App.css";
+import { Header } from "./components/Header/Header";
 
-function App () {
-	return <Header />;
+function App() {
+  return <Header />;
 }
 
 export default App;
