f20008b0c395cbf559f0fc0b3126c44d242f348b
Andrés Siri <andres.siri@hotmail.com>
2022-05-25 23:25:23 -0300

    OT194-18 Setup React Router

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
-<<<<<<< HEAD
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import { Header } from './components/Header/Header';
+import { Footer } from './components/Footer/Footer';
+import { Dashboard } from './pages/Dashboard';
+import { Register } from './pages/Register';
+import { Login } from './pages/Login';
+import { NotFound } from './pages/NotFound';
 
 function App () {
-	return <Header />;
-=======
-import React from 'react'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-
-// TODO page components and delete this
-const Home = () => <h1>Home</h1>
-const News = () => <h1>News</h1>
-const NotFound = () => <h1>NotFound</h1>
-
-const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/news' element={<News />} />
-        <Route path='/not-found' element={<NotFound />} />
-        <Route path='*' element={<Navigate to='/not-found' />} />
-      </Routes>
-    </BrowserRouter>
-  )
->>>>>>> 2faf5988887bd5a1c3b2bef5e0cef23f718e8269
+	return (
+		<Router>
+			<div>
+				<Header />
+				<Routes>
+					<Route path='/' element={<Dashboard />} />
+					<Route path='/login' element={<Login />} />
+					<Route path='/register' element={<Register />} />
+					<Route path='/news' element={<h1>Página de prueba news</h1>} />
+					<Route path='/not-found' element={<NotFound />} />
+					<Route path='*' element={<Navigate to='/not-found' />} />
+				</Routes>
+				<Footer />
+			</div>
+		</Router>
+	);
 }
 
-export default App
+export default App;
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Dashboard () {
+	return (
+		<>
+			<h1>Página de inicio</h1>
+		</>
+	);
+}
+
+export {
+	Dashboard
+};
