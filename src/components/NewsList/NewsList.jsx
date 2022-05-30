7f66da09d0fd2eea978d6cf9c2808bcd34c51092
urreita9 <francisco.urrea9@gmail.com>
2022-05-30 13:18:18 -0300

    NewsList table reponsive

--- src/components/NewsList/NewsList.jsx
+++ src/components/NewsList/NewsList.jsx
@@ -51,26 +51,26 @@ export const NewsList = () => {
 	const handleNewsDelete = (id) => {
 		console.log('Delete id: ', id);
 	};
-	return (<div className='container mx-auto my-20 '>
+	return (<div className='container mx-auto my-20 relative overflow-x-auto' style={{ overflow: scroll }}>
 		<table className="mx-auto shadow-lg bg-white  border border-slate-400 ">
 			<thead className='bg-redOng'>
 				<tr>
-					<th className='bg-blue-100 border text-left px-8 py-4 '>Name</th>
-					<th className='bg-blue-100 border text-left px-8 py-4 '>Image</th>
-					<th className='bg-blue-100 border text-left px-8 py-4 '>Created At</th>
-					<th className='bg-blue-100 border text-left px-8 py-4 '>Update</th>
-					<th className='bg-blue-100 border text-left px-8 py-4 '>Delete</th>
+					<th className='bg-blue-100 border text-left px-8 py-4 text-sm'>Nombre</th>
+					<th className='bg-blue-100 border text-left px-8 py-4 text-sm'>Imagen</th>
+					<th className='bg-blue-100 border text-left px-8 py-4 text-sm'>Fecha Creación</th>
+					<th className='bg-blue-100 border text-left px-8 py-4 text-sm'>Editar</th>
+					<th className='bg-blue-100 border text-left px-8 py-4 text-sm'>Eliminar</th>
 				</tr>
 			</thead>
 			<tbody>
 				{
 					data.map((item, i) =>	(
 						<tr key={i}>
-							<td className='border px-8 py-4 '>{item.name}</td>
-							<td className='border px-8 py-4 '><img className='rounded-full h-12 w-12' src={item.image}/></td>
-							<td className='border px-8 py-4 '>{item.cratedAt}</td>
-							<td className=''><Button className='border-0 px-6 py-2 rounded bg-blueOng text-white cursor-pointer' cb={handleNewsUpdate.bind(null, item.id)} text='Update'/></td>
-							<td className=''><Button className='border-0 px-6 py-2 rounded bg-redOng text-white cursor-pointer' cb={handleNewsDelete.bind(null, item.id)} text='Delete'/></td>
+							<td className='border px-8 py-4 text-sm'>{item.name}</td>
+							<td className='border px-8 py-4 text-sm'><img className='rounded-full h-12 w-12' src={item.image}/></td>
+							<td className='border px-8 py-4 text-sm'>{item.cratedAt}</td>
+							<td className='text-center'><Button className='border-0 px-6 py-2 rounded bg-blueOng text-white cursor-pointer text-sm' cb={handleNewsUpdate.bind(null, item.id)} text='Editar'/></td>
+							<td className='text-center'><Button className='border-0 px-6 py-2 rounded bg-redOng text-white cursor-pointer text-sm' cb={handleNewsDelete.bind(null, item.id)} text='Eliminar'/></td>
 						</tr>))
 				}
 
