21a5f40fb13727b66e266b73c0e2ff81ec2f5baf
Lina Gutierrez Arribas <76691894+linamikaela21@users.noreply.github.com>
2022-05-29 22:43:00 -0300

    Configurar Husky PreCommit ( Frontend ) (#26)
    Co-authored-by: Lali-DMI <lgarribas@dminc.com>

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,17 +1,17 @@
-import React from "react";
-import { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faChevronRight,
-  faChevronLeft,
-} from "@fortawesome/free-solid-svg-icons";
-import styled from "styled-components";
+	faChevronRight,
+	faChevronLeft
+} from '@fortawesome/free-solid-svg-icons';
+import styled from 'styled-components';
 
 const CarouselImg = styled.img`
-  width: 100%;
   height: 246px;
   opacity: 0;
   transition: 1s;
+  width: 100%;
   &.loaded {
     opacity: 1;
   }
@@ -20,10 +20,10 @@ const CarouselImg = styled.img`
 const CarouselDivTxt = styled.div`
   display: flex;
   flex-wrap: wrap;
-  width: 77%;
   margin: 0px auto 5px;
   opacity: 0;
   transition: 1s;
+  width: 77%;
   &.loaded {
     opacity: 1;
   }
@@ -32,66 +32,66 @@ const CarouselDivTxt = styled.div`
   }
 `;
 
-function Slider(props) {
-  const [selectedIndex, setSelectedIndex] = useState(0);
-  const [selectedImage, setSelectedImage] = useState(props.arr[0].imageUrl);
-  const [selectedText, setSelectedText] = useState(props.arr[0].text);
-  const [selectedCampo, setSelectedCampo] = useState(props.arr[0].campo);
-  const [loaded, setLoaded] = useState(false);
+function Slider (props) {
+	const [selectedIndex, setSelectedIndex] = useState(0);
+	const [selectedImage, setSelectedImage] = useState(props.arr[0].imageUrl);
+	const [selectedText, setSelectedText] = useState(props.arr[0].text);
+	const [selectedCampo, setSelectedCampo] = useState(props.arr[0].campo);
+	const [loaded, setLoaded] = useState(false);
 
-  const prev = () => {
-    setLoaded(false);
-    setTimeout(() => {
-      const prevIndex =
+	const prev = () => {
+		setLoaded(false);
+		setTimeout(() => {
+			const prevIndex =
         selectedIndex > 0 ? selectedIndex - 1 : props.arr.length - 1;
-      setSelectedIndex(prevIndex);
-      setSelectedImage(props.arr[prevIndex].imageUrl);
-      setSelectedText(props.arr[prevIndex].text);
-      setSelectedCampo(props.arr[prevIndex].campo);
-    }, 700);
-  };
-  const next = () => {
-    setLoaded(false);
-    setTimeout(() => {
-      const nextIndex =
+			setSelectedIndex(prevIndex);
+			setSelectedImage(props.arr[prevIndex].imageUrl);
+			setSelectedText(props.arr[prevIndex].text);
+			setSelectedCampo(props.arr[prevIndex].campo);
+		}, 700);
+	};
+	const next = () => {
+		setLoaded(false);
+		setTimeout(() => {
+			const nextIndex =
         selectedIndex < props.arr.length - 1 ? selectedIndex + 1 : 0;
-      setSelectedIndex(nextIndex);
-      setSelectedImage(props.arr[nextIndex].imageUrl);
-      setSelectedText(props.arr[nextIndex].text);
-      setSelectedCampo(props.arr[nextIndex].campo);
-    }, 700);
-  };
-  return (
-    <>
-      <div className=" relative my-5">
-        <CarouselImg
-          src={selectedImage}
-          className={loaded ? "loaded" : ""}
-          onLoad={() => setLoaded(true)}
-          alt="novedades"
-        />
+			setSelectedIndex(nextIndex);
+			setSelectedImage(props.arr[nextIndex].imageUrl);
+			setSelectedText(props.arr[nextIndex].text);
+			setSelectedCampo(props.arr[nextIndex].campo);
+		}, 700);
+	};
+	return (
+		<>
+			<div className=" relative my-5">
+				<CarouselImg
+					src={selectedImage}
+					className={loaded ? 'loaded' : ''}
+					onLoad={() => setLoaded(true)}
+					alt="novedades"
+				/>
 
-        <button
-          type="button"
-          onClick={prev}
-          className="flex items-center p-2 border-0 text-base rounded-full text-gray-600 bg-white hover:bg-gray-100 absolute left-5 top-1/2"
-        >
-          <FontAwesomeIcon icon={faChevronLeft} className=" h-3.5 w-3.5" />
-        </button>
-        <button
-          type="button"
-          onClick={next}
-          className="flex items-center p-2 border-0 text-base rounded-full text-gray-600 bg-white hover:bg-gray-100 absolute right-5  top-1/2"
-        >
-          <FontAwesomeIcon icon={faChevronRight} className=" h-3.5 w-3.5" />
-        </button>
-      </div>
-      <CarouselDivTxt className={loaded ? "loaded" : ""}>
-        <h1 className="font-semibold">{selectedCampo}</h1>
-        <p className="w-full text-left  mt-0">{selectedText}</p>
-      </CarouselDivTxt>
-    </>
-  );
+				<button
+					type="button"
+					onClick={prev}
+					className="flex items-center p-2 border-0 text-base rounded-full text-gray-600 bg-white hover:bg-gray-100 absolute left-5 top-1/2"
+				>
+					<FontAwesomeIcon icon={faChevronLeft} className=" h-3.5 w-3.5" />
+				</button>
+				<button
+					type="button"
+					onClick={next}
+					className="flex items-center p-2 border-0 text-base rounded-full text-gray-600 bg-white hover:bg-gray-100 absolute right-5  top-1/2"
+				>
+					<FontAwesomeIcon icon={faChevronRight} className=" h-3.5 w-3.5" />
+				</button>
+			</div>
+			<CarouselDivTxt className={loaded ? 'loaded' : ''}>
+				<h1 className="font-semibold">{selectedCampo}</h1>
+				<p className="w-full text-left  mt-0">{selectedText}</p>
+			</CarouselDivTxt>
+		</>
+	);
 }
 
 export default Slider;
