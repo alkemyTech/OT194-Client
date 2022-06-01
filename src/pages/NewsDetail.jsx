bb9c5648ecb74cbae754d1da077205420271dc4b
MijaelFV <fvmijael@gmail.com>
2022-06-01 17:27:48 -0300

    add exceptions if newsDetails is undefined

--- src/pages/NewsDetail.jsx
+++ src/pages/NewsDetail.jsx
@@ -11,7 +11,7 @@ export const NewsDetail = () => {
 
 	// const { id } = useParams();
 	// const details = useFetch(`/news/${id}`, 'get', {});
-
+	// const details = undefined;
 	const details = {
 		titulo: 'Cupidatat eiusmod sunt nostrud',
 		descripcion: 'Eu ut reprehenderit amet ea Lorem deserunt velit exercitation do consectetur consectetur. Exercitation nulla sunt ut eu qui. Ad officia irure eu dolor enim. Excepteur dolore sint labore sit anim nisi qui quis exercitation esse veniam sunt incididunt. Exercitation id consequat qui elit. Consequat cupidatat consectetur excepteur qui sunt in id ipsum. Veniam occaecat anim irure tempor eu excepteur voluptate Magna duis consectetur aliquip ad fugiat qui sint esse occaecat labore incididunt irure ex. Velit id voluptate reprehenderit fugiat velit exercitation reprehenderit fugiat. Nisi non sint qui esse aute labore aute. Anim sunt est dolore sint minim cillum id tempor Exercitation sint qui aute in enim eiusmod Ullamco proident est deserunt proident cupidatat ea voluptate magna irure ad. Quis nisi aliquip occaecat commodo aute. Sit reprehenderit consectetur cupidatat cupidatat mollit quis ipsum minim mollit. Sit ullamco nulla culpa ad sunt eiusmod mollit proident anim pariatur minim veniam. Mollit tempor nostrud est aliqua culpa irure duis. Officia non veniam incididunt ea exercitation commodo non consequat ipsum eiusmod id ex enim.',
@@ -26,8 +26,8 @@ export const NewsDetail = () => {
 			<h1 className="mx-auto font-sans">Detalles de la novedad</h1>
 			<div className="my-2 w-full overflow-hidden flex items-center bg-neutral-300" style={{ height: 'fit-content', maxHeight: '500px' }}>
 				{
-					details.images.length > 0
-						? <img className="flex" src={details.images[1]} alt={`Imagen sobre ${details.titulo}`} width={'100%'}/>
+					details?.images.length > 0
+						? <img className="flex" src={details?.images[1]} alt={`Imagen sobre ${details?.titulo}`} width={'100%'}/>
 						: <div className="flex bg-neutral-800 font-sans text-white w-full items-center justify-center" style={{ height: '500px' }}>
 							<p>Imagen no encontrada</p>
 						</div>
@@ -35,8 +35,8 @@ export const NewsDetail = () => {
 
 			</div>
 			<div className="flex flex-col px-5 md:px-8">
-				<h2 className="font-sans">{details.titulo}</h2>
-				<p className="m-0 p-0">{details.descripcion}</p>
+				<h2 className="font-sans">{details ? details?.titulo : 'Se ha producido un error inesperado'}</h2>
+				<p className="m-0 p-0">{details ? details?.descripcion : 'La novedad solicitad no fue encontrada.'}</p>
 				<a className="mx-auto sm:mx-0 mb-4 mt-8" href={'/'}>
 					<button className="hover:scale-105 transition-transform appearance-none border border-transparent text-base w-44 text rounded-lg cursor-pointer bg-redOng text-white py-3">
             Volver al inicio
