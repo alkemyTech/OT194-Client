7693cdeb89225679bb3772329b06d9668c12a13a
Martin Farres <91481417+Tats01@users.noreply.github.com>
2022-06-01 23:36:20 -0300

    Edit Form User (#38)

---
+++ src/components/EditUser/index.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { resetAuthReq, getUserData, editUserData } from '../../features/auth/authSlice';
+import { showAlert } from '../../actions/alertsActions';
+import { Formik } from 'formik';
+import { EditUserForm } from '../share/Forms/EditUserForm';
+import { editUserFormSchema } from '../share/Forms/EditUserForm/schemaEditUserForm';
+
+export const EditUserFormik = () => {
+	const [startValues, setStartValues] = useState({
+		firstName: '',
+		lastName: '',
+		roleId: ''
+	});
+
+	const { user, isError, isSuccess, message } = useSelector(
+		(state) => state.auth
+	);
+
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		if (isError) dispatch(showAlert(true, 'Error', message));
+
+		if (isSuccess || user) {
+			navigate('/');
+		};
+
+		dispatch(resetAuthReq());
+	}, [user, isError, isSuccess, message, navigate, dispatch]);
+
+	useEffect(() => {
+		const data = dispatch(getUserData());
+		setStartValues(data);
+	}, [user]);
+
+	return (
+		<>
+			<Formik
+				data-testid="login-test-id-formik"
+				enableReinitialize
+				initialValues={startValues}
+				validationSchema={editUserFormSchema}
+				onSubmit={(values, { setSubmitting }) => {
+					dispatch(editUserData(values));
+					setSubmitting(false);
+				}}
+				component={EditUserForm}
+				validateOnChange={false}
+				validateOnBlur={false}
+			/>
+		</>
+	);
+};
