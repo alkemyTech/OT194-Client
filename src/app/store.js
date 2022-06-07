4414fa82a07ca3c643566c55989d0542fb32cf60
MijaelFV <fvmijael@gmail.com>
2022-06-07 20:19:43 -0300

    add news formulary

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import alertsReducer from '../reducers/alertsReducer';
 import authSlice from '../features/auth/authSlice';
 import activitiesSlice from '../features/activities/activitiesSlice';
+import newsSlice from '../features/news/newsSlice';
 
 export default configureStore({
 	reducer: {
 		alerts: alertsReducer,
+		news: newsSlice,
 		auth: authSlice,
 		activities: activitiesSlice
 	}
