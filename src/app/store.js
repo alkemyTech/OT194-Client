6f5faf680e4fdcaed2c715f51addbfc80c364ab7
MijaelFV <fvmijael@gmail.com>
2022-06-09 13:12:36 -0300

    add testimonials form

--- src/app/store.js
+++ src/app/store.js
@@ -4,6 +4,7 @@ import activitiesSlice from '../features/activities/activitiesSlice';
 import alertSlice from '../features/alert/alertSlice';
 import newsSlice from '../features/news/newsSlice';
 import componentsSlice from '../features/components/componentsSlice';
+import testimonialssSlice from '../features/testimonials/testimonialsSlice';
 
 export default configureStore({
 	reducer: {
@@ -11,6 +12,7 @@ export default configureStore({
 		news: newsSlice,
 		auth: authSlice,
 		activities: activitiesSlice,
-		components: componentsSlice
+		components: componentsSlice,
+		testimonials: testimonialssSlice
 	}
 });
