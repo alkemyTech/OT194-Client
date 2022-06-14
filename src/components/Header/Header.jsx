f7d95eaad720986d1fa0a2fd06d1e034bdb68eb0
Andrés Siri <andres.siri@hotmail.com>
2022-06-14 00:36:07 -0300

    OT194-102 Modify styles according to ONG logo image

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -17,10 +17,10 @@ export function Header () {
 	};
 
 	return (
-		<nav className="h-16 w-full fixed box-border shadow-md flex items-center justify-between bg-white pl-6 pr-6 py-0 select-none">
+		<nav className="h-16 w-full z-50 fixed box-border shadow-md flex items-center justify-between bg-white pl-6 pr-6 py-0 select-none">
 			<div className="flex items-center flex-shrink-0 text-white mr-6">
 				<img
-					className="cursor-pointer mx-auto my-0 w-24 h-14"
+					className="cursor-pointer mx-auto my-0 w-24 h-14 object-cover"
 					src='/logo-fundacion.png'
 					onClick={handleLogo}
 				/>
