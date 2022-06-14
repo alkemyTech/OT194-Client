a18098a09b4756ebfdc286b1d1bf52c3f37fe299
Martin Farres <91481417+Tats01@users.noreply.github.com>
2022-06-14 20:08:30 -0300

    Fix Mobile View - Componente 'News'

--- src/components/News/index.js
+++ src/components/News/index.js
@@ -5,23 +5,23 @@ import PropTypes from 'prop-types';
 import { getAllNews } from '../../features/news/newsSlice';
 
 const Card = ({ data }) => (
-	<div className="w-5/6 mb-3 shadow-lg rounded-3xl border-solid border border-blue-700" style={{ backgroundColor: '#7E9AFD' }}>
-		<div className="">
+	<div className="w-max shadow-lg flex bg-blueOng rounded-3xl mx-5 border-solid border  border-blue-700" style={{ backgroundColor: '#7E9AFD' }}>
+		<div className="flex-no-shrink w-1/2">
 			<img
 				alt={data.name}
-				className="block mx-auto rounded-3xl p-2 h-44 w-fit"
+				className="block mx-auto rounded-3xl p-2 h-32 w-32"
 				src={data.image}
 			/>
 		</div>
-		<div className="flex flex-col justify-between">
-			<p className="font-medium px-1">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae augue augue.  varius laoreet nibh.
-			</p>
-			<Link to={`/news/${data.id}`} >
-				<button className=' mb-2 w-11/12 font-mulish  appearance-none border rounded-xl border-transparent text-white p-2 py-2.5 text-center no-underline bg-blue-700 ease-in-out duration-200  hover:bg-white hover:border-blue-700 hover:text-black ' style={{ boxShadow: '0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)' }}>
+		<div className="max-w-lg flex flex-wrap-reverse justify-between">
+			<div>
+				<h3 className="font-medium p-2">{data.name}</h3>
+				<Link to={`/news/${data.id}`}>
+					<button className="w-32 m-2 font-englebert bg-blue-700  text-white font-mulish appearance-none py-2 px-4 border rounded-xl border-transparent  roundedbg-blue-700 ease-in-out duration-200  hover:bg-white hover:border-blue-700 hover:text-black" style={{ boxShadow: '0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)' }}>
 						Ver Novedad
-				</button>
-			</Link>
+					</button>
+				</Link>
+			</div>
 		</div>
 	</div>
 );
@@ -34,9 +34,9 @@ export const News = () => {
 	}, []);
 
 	return (
-		<div className="p-4">
-			<h1 className="flex justify-start font-bold ">Últimas novedades</h1>
-			<div className="flex flex-wrap ">
+		<div className="p-4 justify-center">
+			<h1 className="flex justify-center font-bold ">Últimas novedades</h1>
+			<div className="grid grid-cols-1 gap-4 justify-items-center sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
 				{allNews.map(data => (
 					<Card key={data.id} data={data} />
 				))}
