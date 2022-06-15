ed9515165a46c16c4c70b8cce50538b960579191
Martin Farres <91481417+Tats01@users.noreply.github.com>
2022-06-14 23:39:16 -0300

    Ot194-108  -  Página Novedades / Detalle de Novedad - Revisión de diseño (#59)
    * Revision Pagina Novedades
    
    * Fix Mobile View - Componente 'News'

--- src/components/News/index.js
+++ src/components/News/index.js
@@ -1,36 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { getAllNews } from '../../features/news/newsSlice';
 
-export const News = () => {
-	const news = [{ id: '123', name: 'Novedad', image: 'https://source.unsplash.com/WLUHO9A_xik/1600x900' }, { id: '124', name: 'Novedad2', image: 'https://source.unsplash.com/WLUHO9A_xik/1600x900' }, { id: '125', name: 'Novedad3', image: 'https://source.unsplash.com/WLUHO9A_xik/1600x900' }];
-
-	const Card = ({ data }) => (
-		<div className="w-max shadow-lg flex bg-blueOng rounded-3xl mx-5">
-			<div className="flex-no-shrink w-1/2">
-				<img
-					alt={data.name}
-					className="block mx-auto rounded-3xl p-2 h-32 w-32"
-					src={data.image}
-				/>
-			</div>
-			<div className="max-w-lg">
-				<div>
-					<h3 className="font-medium p-2">{data.name}</h3>
-					<Link to={`news/${data.id}`}>
-						<button className="w-32 m-2 font-englebert bg-bluePure hover:bg-blueOng text-white font-bold py-2 px-4 border border-bluePure rounded">
-Ver Novedad
-						</button>
-					</Link>
-				</div>
+const Card = ({ data }) => (
+	<div className="w-max shadow-lg flex bg-blueOng rounded-3xl mx-5 border-solid border  border-blue-700" style={{ backgroundColor: '#7E9AFD' }}>
+		<div className="flex-no-shrink w-1/2">
+			<img
+				alt={data.name}
+				className="block mx-auto rounded-3xl p-2 h-32 w-32"
+				src={data.image}
+			/>
+		</div>
+		<div className="max-w-lg flex flex-wrap-reverse justify-between">
+			<div>
+				<h3 className="font-medium p-2">{data.name}</h3>
+				<Link to={`/news/${data.id}`}>
+					<button className="w-32 m-2 font-englebert bg-blue-700  text-white font-mulish appearance-none py-2 px-4 border rounded-xl border-transparent  roundedbg-blue-700 ease-in-out duration-200  hover:bg-white hover:border-blue-700 hover:text-black" style={{ boxShadow: '0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)' }}>
+						Ver Novedad
+					</button>
+				</Link>
 			</div>
 		</div>
-	);
+	</div>
+);
+
+export const News = () => {
+	const dispatch = useDispatch();
+	const allNews = useSelector(state => state.news.allNews);
+	useEffect(() => {
+		dispatch(getAllNews());
+	}, []);
+
 	return (
-		<div className="p-4">
-			<h1 className="flex justify-start font-bold p-4">Últimas novedades</h1>
-			<div className="grid grid-cols-4 gap-4">
-				{news.map(data => (
+		<div className="p-4 justify-center">
+			<h1 className="flex justify-center font-bold ">Últimas novedades</h1>
+			<div className="grid grid-cols-1 gap-4 justify-items-center sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+				{allNews.map(data => (
 					<Card key={data.id} data={data} />
 				))}
 			</div>
@@ -38,9 +45,6 @@ Ver Novedad
 	);
 };
 
-News.propTypes = {
-	data: PropTypes.object,
-	name: PropTypes.string,
-	image: PropTypes.string,
-	id: PropTypes.string
+Card.propTypes = {
+	data: PropTypes.object
 };
--- src/pages/NewsDetail.jsx
+++ src/pages/NewsDetail.jsx
@@ -1,14 +1,10 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import Slider from '../components/Slider';
 import { getNews } from '../features/news/newsSlice';
-// import { useParams } from 'react-router-dom';
-// import { useFetch } from '../hooks/useFetch';
 
 export const NewsDetail = () => {
-	// No se utilizo el Slider porque renderiza las imagenes estiradas y sin posibilidad de corregirlo ademas de que tiene un bug que desaparece la imagen
-	// Deberian sacarse los argumentos "text" y "campo" del Slider ya que este componente solo debe encargarse de imagenes
-
 	const dispatch = useDispatch();
 	const newsDetails = useSelector(state => state.news.openedNews);
 
@@ -19,21 +15,15 @@ export const NewsDetail = () => {
 	}, [newsId]);
 
 	return (
+
 		<div className="flex flex-col mx-auto" style={{ maxWidth: '1200px' }} >
 			<h1 className="mx-auto font-sans">Detalles de la novedad</h1>
-			<div className="my-2 w-full overflow-hidden flex items-center bg-neutral-300" style={{ height: 'fit-content', maxHeight: '500px' }}>
-				{
-					newsDetails?.images?.length > 0
-						? <img className="flex" src={newsDetails?.images[1]} alt={`Imagen sobre ${newsDetails?.titulo}`} width={'100%'}/>
-						: <div className="flex bg-neutral-800 font-sans text-white w-full items-center justify-center" style={{ height: '500px' }}>
-							<p>Imagen no encontrada</p>
-						</div>
-				}
-
+			<div className="my-2 w-full overflow-hidden flex items-center " style={{ height: 'fit-content', maxHeight: '500px' }}>
+				<Slider arr={newsDetails?.data?.Slides || []}/>
 			</div>
 			<div className="flex flex-col px-5 md:px-8">
-				<h2 className="font-sans">{newsDetails.length > 0 ? newsDetails?.titulo : 'Se ha producido un error inesperado'}</h2>
-				<p className="m-0 p-0">{newsDetails.length > 0 ? newsDetails?.descripcion : 'La novedad solicitad no fue encontrada.'}</p>
+				<h2 className="font-sans">{ newsDetails?.data?.name || 'Se ha producido un error inesperado'}</h2>
+				<p className="m-0 p-0">{ newsDetails?.data?.content || 'La novedad solicitad no fue encontrada.'}</p>
 				<a className="mx-auto sm:mx-0 mb-4 mt-8" href={'/'}>
 					<button className="hover:scale-105 transition-transform appearance-none border border-transparent text-base w-44 text rounded-lg cursor-pointer bg-redOng text-white py-3">
             Volver al inicio
