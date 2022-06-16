a3a6c5237fa7bc2a7be70514559e27ccbb5b6d04
MijaelFV <fvmijael@gmail.com>
2022-06-16 16:30:11 -0300

    fix news card

--- src/components/News/index.js
+++ src/components/News/index.js
@@ -5,23 +5,26 @@ import PropTypes from 'prop-types';
 import { getAllNews } from '../../features/news/newsSlice';
 
 const Card = ({ data }) => (
-	<div className="w-max shadow-lg flex bg-blueOng rounded-3xl mx-5 border-solid border  border-blue-700" style={{ backgroundColor: '#7E9AFD' }}>
-		<div className="flex-no-shrink w-1/2">
+	<div
+		className="shadow-lg grid grid-cols-2 space-x-3 p-3 overflow-hidden bg-blueOng rounded-3xl box-border border-solid border border-blue-700"
+		style={{ backgroundColor: '#7E9AFD', width: '448px', height: '233px' }}
+	>
+		<div className="box-content overflow-hidden">
 			<img
 				alt={data.name}
-				className="block mx-auto rounded-3xl p-2 h-32 w-32"
+				className="rounded-3xl h-full w-full"
 				src={data.image}
 			/>
 		</div>
-		<div className="max-w-lg flex flex-wrap-reverse justify-between">
-			<div>
-				<h3 className="font-medium p-2">{data.name}</h3>
-				<Link to={`/news/${data.id}`}>
-					<button className="w-32 m-2 font-englebert bg-blue-700  text-white font-mulish appearance-none py-2 px-4 border rounded-xl border-transparent  roundedbg-blue-700 ease-in-out duration-200  hover:bg-white hover:border-blue-700 hover:text-black" style={{ boxShadow: '0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)' }}>
+		<div className="flex flex-col justify-between">
+			<p className="m-0 font-medium text-base text-left" >
+				{data.content.slice(0, 150)}
+			</p>
+			<Link to={`/news/${data.id}`}>
+				<button className="cursor-pointer drop-shadow-md w-full bg-blue-700 text-white appearance-none py-2.5 border rounded-lg border-transparent ease-in-out duration-200 hover:bg-white hover:border-blue-700 hover:text-black">
 						Ver Novedad
-					</button>
-				</Link>
-			</div>
+				</button>
+			</Link>
 		</div>
 	</div>
 );
@@ -34,9 +37,11 @@ export const News = () => {
 	}, []);
 
 	return (
-		<div className="p-4 justify-center">
-			<h1 className="flex justify-center font-bold ">Últimas novedades</h1>
-			<div className="grid grid-cols-1 gap-4 justify-items-center sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+		<div className="p-4 mx-auto"
+			style={{ maxWidth: '1500px' }}
+		>
+			<h1>Últimas novedades</h1>
+			<div className="flex flex-wrap gap-4 justify-center">
 				{allNews.length && allNews.map(data => (
 					<Card key={data.id} data={data} />
 				))}
