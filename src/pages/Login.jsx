2e777039ca2d8313119a7b61ce6a771f68a2d608
MijaelFV <fvmijael@gmail.com>
2022-06-16 18:32:06 -0300

    few login & register styles changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,7 +12,13 @@ export const Login = () => {
 			{isLoading && <Spinner />}
 			<div className='flex items-center justify-center w-full box-content'>
 				<div className='flex flex-col items-center justify-center w-full h-full md:w-6/12 my-14 md:my-0 p-2'>
-					<div className='flex flex-col justify-center items-start sm:w-4/6'>
+					<div
+						className='flex flex-col justify-center items-start w-full'
+						style={{
+							width: '90%',
+							maxWidth: '450px'
+						}}
+					>
 						<p className='mb-0'>Bienvenido</p>
 						<h1 className='text-2xl xs:text-3xl mt-0'>Inicia sesión en tu cuenta!</h1>
 						<LoginFormik />
@@ -24,9 +30,10 @@ export const Login = () => {
 						</Link>
 					</p>
 				</div>
-				<div className='hidden md:flex md:w-6/12 max-height-screen object-cover'>
+				<div className='hidden md:flex md:w-6/12 overflow-hidden'>
 					<img
-						className='w-full h-full object-cover object-left max-height-screen'
+						style={{ height: '800px' }}
+						className='w-full h-full object-cover max-height-screen rounded-r-lg'
 						src='images/login-hands.png'
 						alt='manos unidas'
 					/>
