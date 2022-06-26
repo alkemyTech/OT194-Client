54d81c63e09e5400560209547f24e0cf54f5890b
MijaelFV <fvmijael@gmail.com>
2022-06-26 20:42:44 -0300

    fix news form character limit

--- src/components/share/Forms/NewsForm/schemaNewsForm.js
+++ src/components/share/Forms/NewsForm/schemaNewsForm.js
@@ -1,8 +1,9 @@
 import * as Yup from 'yup';
 const required = 'es un campo requerido';
-const length = 'debe tener al menos 6 caracteres';
+const maxLength = 'debe tener menos caracteres';
+const minLength = 'debe tener más caracteres';
 
 export const newsFormSchema = Yup.object().shape({
-	name: Yup.string().min(6, `El titulo ${length}`).required(`El titulo ${required}`),
-	content: Yup.string().required(`El contenido ${required}`)
+	name: Yup.string().min(6, `El titulo ${minLength}`).required(`El titulo ${required}`),
+	content: Yup.string().max(240, `El contenido  ${maxLength}`).required(`El contenido ${required}`)
 });
